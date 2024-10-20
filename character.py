6dc36bd9d3c719689f9c3b31ea3abd2ff0865afa
Amsellem--Bousignac Ryan <ryvida@hotmail.fr>
2024-10-20 14:36:56 +0200

    can't go out of the map + critic and miss stat

--- character.py
+++ character.py
@@ -1,5 +1,5 @@
 class Character :
-  def __init__(self, name, level, health, attack, defense, postion_x, postion_y):
+  def __init__(self, name, level, health, attack, defense, postion_x, postion_y, critic_hit, miss_hit):
     self.name = name
     self.level = level
     self.health = health
@@ -8,16 +8,18 @@ class Character :
     self.inventory = []
     self.postion_x = postion_x
     self.postion_y = postion_y
+    self.critic_hit = critic_hit
+    self.miss_hit = miss_hit
 
 class Player (Character):
   def __init__(self, name):
     super().__init__(name,1,100,10,5,3,3)
 
 class Monster (Character):
-  def __init__(self, name, level, health, attack, defense, postion_x, postion_y):
-    super().__init__(name,level, health, attack, defense, postion_x, postion_y)
+  def __init__(self, name, level, health, attack, defense, postion_x, postion_y, critic_hit, miss_hit):
+    super().__init__(name,level, health, attack, defense, postion_x, postion_y, critic_hit, miss_hit)
 
 class Boss (Monster):
-  def __init__(self, name, level, health, attack, defense, postion_x, postion_y):
-    super().__init__(name,level, health, attack, defense, postion_x, postion_y)
+  def __init__(self, name, level, health, attack, defense, postion_x, postion_y, critic_hit, miss_hit):
+    super().__init__(name,level, health, attack, defense, postion_x, postion_y, critic_hit, miss_hit)
     self.boss_dead = False
--- game.py
+++ game.py
@@ -6,35 +6,44 @@ def start_game(player):
     while player.health > 0 or boss.boss_dead == True:
         player_input = (input("Your movement : "))
         
-        if player_input == "up" or player_input == "UP":
-            print(player.postion_y)
-            player.postion_y = player.postion_y - 1
-            print(player.postion_y)
-            verify_player_position(player)
+        old_x, old_y = player.postion_x, player.postion_y
+        
+        if player_input.lower() == "up":
+            player.postion_y -= 1
             
-        elif player_input == "down" or player_input == "DOWN":
-            player.postion_y = player.postion_y + 1
-            verify_player_position(player)
+        elif player_input.lower() == "down":
+            player.postion_y += 1
             
-        elif player_input == "right" or player_input == "RIGHT":
-            player.postion_x = player.postion_x + 1
-            verify_player_position(player)
+        elif player_input.lower() == "right":
+            player.postion_x += 1
             
-        elif player_input == "left" or player_input == "LEFT":
-            player.postion_x = player.postion_x - 1
-            verify_player_position(player)
+        elif player_input.lower() == "left":
+            player.postion_x -= 1
             
         else:
             print("Please enter a good direction")
-
+            continue
+        
+        if not verify_player_position(player):
+            player.postion_x, player.postion_y = old_x, old_y
+            print("You hit a Wall, you don't move of your case and please choose another direction")
 
 def verify_player_position(player):
+    
     print("x : ", player.postion_x, " y : ", player.postion_y )
     if player.postion_x >= 3 and player.postion_y >= 0 and player.postion_x <= 6 and player.postion_y <= 6:
         print("You are in the Forest")
+        return True
         
     if player.postion_x >= 0 and player.postion_y >= 0 and player.postion_x <= 2 and player.postion_y <= 3:
         print("You are in the Dungeon")
+        return True
         
     if player.postion_x >= 0 and player.postion_y >= 4 and player.postion_x <= 2 and player.postion_y <= 6:
         print("You are in the Cave")
+        return True
+    
+    if player.postion_x < 0 or player.postion_y < 0 or player.postion_x > 6 or player.postion_y > 6:
+        return False
+    
+    return True
