d7e7689a1c5029f9df3e43af85922e142eaf140b
Amsellem--Bousignac Ryan <ryvida@hotmail.fr>
2024-10-29 15:18:52 +0100

    add objects and monsters

--- Game_logic/data.py
+++ Game_logic/data.py
@@ -14,15 +14,19 @@ boss = Boss("Dragon",10,1000,50,20,0,0,25,4,25, 1000, "WoW, The dragon burned yo
 # Creation of all of the objects
 list_objects = [
     health_potion("health potion", 10, 2, 2),
+    health_potion("health potion", 10, *random_position_without_exclude_coordinates(forbidden_coords)),
     attack_potion("attack potion", 10, *random_position_without_exclude_coordinates(forbidden_coords)),
+    attack_potion("attack potion", 10, *random_position_without_exclude_coordinates(forbidden_coords)),
+    defense_potion("defense potion", 5, *random_position_without_exclude_coordinates(forbidden_coords)),
     defense_potion("defense potion", 5, *random_position_without_exclude_coordinates(forbidden_coords))
 ]
 
 # Creation of all of the monsters
 list_monster = [
     Monster("Wolf",1, 50, 5, 2, 1,1, 10, 1.5, 20, 150, "Oh the wolf bite your head !!"),
-    Monster("Wolf",1, 50, 5, 2, 4,4, 10, 1.5, 20, 20, "Oh the wolf bite your head !!"),
-    Monster("Wolf",1, 50, 5, 2, 5,5, 10, 1.5, 20, 70, "Oh the wolf bite your head !!")
+    Monster("Gobelin",2, 75, 10, 4, *random_position_without_exclude_coordinates(forbidden_coords), 12, 1.7, 18, 200, "Oh the gobelin cut you with his sword !!"),
+    Monster("Orc",4, 100, 15, 6, *random_position_without_exclude_coordinates(forbidden_coords), 18, 2, 22, 250, "Wow the orc cut you up with his axe !!"),
+    Monster("Elf",5, 115, 18, 7, *random_position_without_exclude_coordinates(forbidden_coords), 18, 1.7, 18, 250, "Incredible the elf hit you with his arrow !!")
 ]
 
 list_monster.append(boss)
